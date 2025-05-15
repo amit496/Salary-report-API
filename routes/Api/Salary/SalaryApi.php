e985843bf34a6a5d5b49f66d49c7b09007ba301c
Rays Coding <gautamamit557@gmail.com>
2025-05-15 18:14:15 +0530

    Postman Collection

--- routes/Api/Salary/SalaryApi.php
+++ routes/Api/Salary/SalaryApi.php
@@ -4,6 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Salary\SalaryReportController;
 
-Route::controller([SalaryReportController::class])->prefix('salray')->group(function (){
-    Route::get('/report', [SalaryReportController::class, 'index']);
-});
+Route::get('salary-report', [SalaryReportController::class, 'index']);
